1b57c50fc3bf0365c4532c3a0f53c43871b1727f
SogogiBoB <jjh9240@naver.com>
2022-05-30 00:33:58 +0900

    jpa

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional //Jpa는 모든 데이터 변경이 트랜젝션 안에서 실행되어야 하기 때문에 꼭 필요함
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -32,7 +34,7 @@ public class MemberService {
     private void validateDulicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원임.");
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
